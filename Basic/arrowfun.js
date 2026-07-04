6e34718c356cc432adf57b8051e6679f358b6b3b
snehasharma9326 <snehasharma9326@gmail.com>
2026-07-04 19:52:40 +0530

    arrow

---
+++ Basic/arrowfun.js
@@ -0,0 +1,67 @@
+let arrowfun = (para1,para2) => {
+    console.log("arrow function")
+    console.log(para1)
+    console.log(para2)
+}
+
+
+// arrowfun("abc","ade")
+// let multiply = (a,b)=>{a*b}
+// {
+//     console.log(multiply(5,10))
+// }
+// multiply(5,10)
+//q1.
+// function numbers(n){
+//     for(let i=1;i<=n;i++){
+//         console.log(i)
+//     }
+// }
+// console.log("print 1 to 5 number");
+// numbers(5)
+// //q2.
+// function evenorodd(n){
+//     if(n%2==0){
+//         return "even";
+//     }else{
+//        return "odd";
+//     }
+// }
+// console.log("check even or odd");
+
+// let result = evenorodd(6)
+
+// if (result==even){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+// Q3 
+// let name=prompt("enter message"){
+    
+// }
+
+
+// 1st
+
+function hello()
+{
+    console.log("hello");
+    
+}
+
+
+// 2nd
+
+var hello1 = function()
+{
+    console.log("he;llo 1")
+}
+
+
+// 3rd 
+arrowfun("")
+
+    let multiply=("a ")=>("s")
