a181b453e1176eab8c3ee75c13aa347c1c4a2b9d
snehasharma9326 <snehasharma9326@gmail.com>
2026-08-06 12:52:01 +0530

    commit

---
+++ Basic/object.js
@@ -0,0 +1,54 @@
+//object creation
+// const person = {
+//     firstname: "shalini",
+//     age:19,
+//     state: "mp",
+//     district: "sehore",
+//     prints: function(){
+//         console.log(`my name is ${this.firstname} district ${this.district} and my age is ${this.age}`);
+//     }
+// }
+//added key
+//person.district = "sehore"
+//accessing values 
+// console.log(person.age);
+// console.log(person['age']);
+
+//updating keys 
+// person.age = 10
+//output 
+// console.log(person)
+// console.log(person.prints())
+
+// for(let key in person){
+//     console.log(key,person[key])
+// }
+
+// Object.keys(person).forEach(key => {
+//     console.log(key)
+// })
+
+// Object.values(person).forEach(values => {
+//     console.log(values)
+// })
+// Object.entries(person).forEach(([key,values])=> {
+//     console.log(key,values);
+// });
+
+// //object creation 
+// const sneha = {
+//     subject:"maths";
+//     Array.
+// }
+
+//document object//
+console.log(document.getElementById("para")).innerHTML = "hello"
+
+
+// console.log(document.getElementsByClassName("cls"))
+
+// console.log(document.getElementsByTagName("a"))
+
+//methods//
+console.log(document.querySelector("#para")) //id
+console.log(document.querySelectorAll(".cls"))   //class
